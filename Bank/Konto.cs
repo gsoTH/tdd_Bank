90ab9d4fba3a247ef767080456877765b80abd7a
M.Thomas <m.thomas@gso.schule.koeln>
2022-11-20 14:54:32 +0100

    Projektdateien hinzufügen.

---
+++ Bank/Konto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Bank
+{
+    internal class Konto
+    {
+
+        private int guthaben;
+
+        public int Guthaben
+        {
+            get
+            {
+                return guthaben;
+            }
+        }
+
+        public Konto(int guthaben)
+        {
+            this.guthaben = guthaben;
+        }
+
+        public void Einzahlen(int betrag)
+        {
+            guthaben += betrag;
+        }
+
+        public void Auszahlen(int betrag)
+        {
+            if (guthaben >= betrag)
+            {
+                guthaben -= betrag;
+            }
+            else
+            {
+                throw new Exception("Guthaben nicht ausreichend");
+            }
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bank
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            // In dieser Datei muss eigentlich nichts bearbeitet werden. 
+            // Wir brauchen nur Tests.
+            Console.WriteLine("Hello World");
+        }
+    }
+}
